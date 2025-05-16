52e20cb68f5bc316368943758a27e6fdacdb3a46
MartimMarques-PT <martimlmarques@gmail.com>
2025-05-16 12:38:18 +0100

    fiz o passo 1, 2, 3 e 4

--- jogo.js
+++ jogo.js
@@ -8,6 +8,32 @@ function getComputerChoice(){
         return "scissors"
     }
 }
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let n1 = prompt("Write your what you want between rock, paper and scissors");
+    let message = "your choice is $(n1)";
+}
+
+function playRound(humanChoice, computerChoice){
+    humanChoice.toLowerCase() = gethumanChoice();
+    computerChoice = getComputerChoice();
+    if (computerChoice == "rock" && humanChoice == "rock"){
+        return "It's a draw!";
+    } else if (computerChoice == "rock" && humanChoice == "paper"){
+        return "You win!";
+    } else if (computerChoice == "rock" && humanChoice == "scissors"){
+        return "You lose!";
+    } else if (computerChoice == "paper" && humanChoice == "rock"){
+        return "You lose!";
+    } else if (computerChoice == "paper" && humanChoice == "paper"){
+        return "It's a draw!";
+    } else if (computerChoice == "paper" && humanChoice == "scissors"){
+        return "You win!";
+    } else if (computerChoice == "scissors" && humanChoice == "rock"){
+        return "You lose!";
+    } else if (computerChoice == "scissors" && humanChoice == "paper"){
+        return "It's a draw!";
+    } else if (computerChoice == "scissors" && humanChoice == "rock"){
+        return "You win!";
+    }
+}
