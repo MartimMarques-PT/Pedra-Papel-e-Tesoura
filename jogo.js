33b5687de880c6fe35b0667c0e49f3bc6e051c4b
MartimMarques-PT <martimlmarques@gmail.com>
2025-05-16 10:45:23 +0100

    fiz o index.html e o jogo.js

--- jogo.js
+++ jogo.js
@@ -1,1 +1,13 @@
-
+function getComputerChoice(){
+    let n = Math.random();
+    if (n <= 0.33){
+        return "rock";
+    } else if (n <= 0.66){
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
